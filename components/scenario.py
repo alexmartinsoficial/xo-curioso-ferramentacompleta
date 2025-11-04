8925836bc39a2ad3cb695dd9e1f53a2b77e03624
alexmartinsoficial <alexmartinsgyn@gmail.com>
2025-11-04 07:39:28 -0300

    Update scenario.py

--- components/scenario.py
+++ components/scenario.py
@@ -13,7 +13,6 @@ class Scenario:
         self.steps = self.data['steps']
         
     @staticmethod
-    @st.cache_data
     def load_scenarios():
         """Carrega todos cenários dos 4 arquivos JSON (com cache)"""
         all_scenarios = {}
